2a0fd06717ce237fe523057a23e6375f8008f0e2
Erlinis Quintana <erlinis.quintana@gmail.com>
2018-11-12 20:09:27 -0500

    Reading command and args with Yargs and listing table

--- 03-NODE-BASICS/app.js
+++ 03-NODE-BASICS/app.js
@@ -5,18 +5,58 @@
  * 2. Scripts written by our own and are located inside the app. Start with ./ followed for the filepath.
  */
 
-const {createMultiplicationTable} = require('./multiply/multiply');
+const {createTable, listTable} = require('./multiply/multiply');
 
+const argv = require('yargs')
+  .command('list', 'Prints the multiplication table of a given base', {
+    base: {
+      demand: true,
+      alias: 'b'
+    },
+    limit: {
+      alias: 'l',
+      default: 10
+    }
+  })
+  .command('create', 'Create the multiplication table of a given base and until the given limit, default 10.', {
+    base: {
+      demand: true,
+      alias: 'b'
+    },
+    limit: {
+      alias: 'l',
+      default: 10
+    }
+  })
+  .help()
+  .argv;
 
-/**
- * process.argv contain an array with all the arguments passed to the app
- * when it is executed from the command line
- */
-let args = process.argv;
-let base = args[2].split('=')[1]
+//console.log(argv);
 
-createMultiplicationTable(base)
- .then(filename => console.log(`File ${filename} created`))
- .catch(e => console.log(e));
+let command = argv._[0];
+
+switch (command) {
+  case 'list':
+    var base = argv.base;
+    var limit = argv.limit;
+
+    listTable(base, limit)
+     .then(data => console.log(data))
+     .catch(e => console.log(e));
+  break;
+
+  case 'create':
+    var base = argv.base;
+    var limit = argv.limit;
+
+    createTable(base, limit)
+     .then(filename => console.log(`File ${filename} created`))
+     .catch(e => console.log(e));
+  break;
+
+  default:
+    console.log('Invalid command');
+  break;
+}
 
 
--- 03-NODE-BASICS/multiply/multiply.js
+++ 03-NODE-BASICS/multiply/multiply.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
-let buildMultiplicationTable = (base) => {
+let buildMultiplicationTable = (base, limit) => {
   return new Promise((resolve, reject) => {
     let data = '';
 
-    for (let i = 1; i <= 10; i++ ) {
+    for (let i = 1; i <= limit; i++ ) {
       data +=(`${base} * ${i} = ${base*i}\n`);
     }
     resolve(data);
   });
 }
 
-let writeMultiplicationTableOnFile = async(data, base) => {
-  var filename = `table-${base}.txt`
+let writeMultiplicationTableOnFile = async(data, base, limit) => {
+  var filename = `table-${base}-to-${limit}.txt`
   fs.writeFile(`tables/${filename}`, data, (err) => {
      if (err) {
       return err;
@@ -21,18 +21,34 @@ let writeMultiplicationTableOnFile = async(data, base) => {
   return filename;
 }
 
-let createMultiplicationTable = async(base) => {
+let createTable = async(base, limit) => {
   if (!Number(base)){
      throw new Error(`The base value: '${base}' is not a number`);
   } else {
-    let data = await buildMultiplicationTable(base);
-    let filename = await writeMultiplicationTableOnFile(data, base);
+    let data = await buildMultiplicationTable(base, limit);
+    let filename = await writeMultiplicationTableOnFile(data, base, limit);
     return filename;
   }
 }
 
+let listTableFile = async(base, limit) => {
+   let data = fs.readFileSync(`tables/table-${base}.txt`, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+    return data;
+  });
+  let truncatedData = data.split('\n').slice(0, limit)
+  return truncatedData.join('\n');
+}
+
+let listTable = async(base, limit) => {
+  return buildMultiplicationTable(base, limit);
+}
+
 module.exports = {
- createMultiplicationTable
+ createTable,
+ listTable
 }
 
 
