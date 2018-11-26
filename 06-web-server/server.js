f128f514b2414cfef61bb4e9f0d2e2146dbef3d0
Erlinis Quintana <erlinis.quintana@gmail.com>
2018-11-25 23:25:23 -0500

    Serving static content and using handelbars for partials and dinamic views

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,11 +1,24 @@
 const express = require('express')
-const app = express()
+const app = express();
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World Express');
-// });
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    user: 'Erlinis',
+    year: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    year: new Date().getFullYear()
+  });
+});
 
 app.get('/data', function (req, res) {
   let output = {
